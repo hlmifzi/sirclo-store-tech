db934a8c1e2edc30cb8c483ed44254790bf55071
Helmi <hlmifzi@gmail.com>
2020-09-05 04:04:20 +0700

    layout until footer

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 import useHome from './hook/useHome'
 import { useDidMount } from '../../utils/componentLifeCycle'
 
-import { Header, Carousel, Slicker, Modal, image1, image2, image3, image4, image5, image6 } from '../../components/base'
+import { Header, Carousel, Slicker, Modal, image1, image2, image3, image4, image5, image6, logoFooter } from '../../components/base'
 
 import './home.scss'
 const Home = () => {
@@ -45,13 +45,13 @@ const Home = () => {
                 </div>
             </section>
 
-            <section className="flex justify-center items-center subcribe__section ">
+            <section className="flex justify-center flex-wrap items-center subcribe__section ">
                 <div className="">icon</div>
                 <div className="">
                     <input />
                 </div>
             </section>
-            <section className="flex justify-center flex-wrap items-center image__section ">
+            <section className="flex flex-wrap items-center image__section ">
                 <img src={image1} alt="" />
                 <img src={image2} alt="" />
                 <img src={image3} alt="" />
@@ -60,7 +60,53 @@ const Home = () => {
                 <img src={image6} alt="" />
             </section>
             <footer>
-
+                <section className="flex px-16 pt-20 flex-wrap justify-between footer__section ">
+                    <div className="description">
+                        <img src={logoFooter} alt="" />
+                        <o>Mavelin is a WordPress E-Commerce theme focused to collecting, grouping your content Then selling your stuffs. it's perfect for small-to-high traffic e-commercebussiness site,</o>
+                        <div>COPYRIGHTS 2017 MAVELIN POWERED BY SIRCLO</div>
+                    </div>
+                    <div className="flex flex-col justify-between pb-20 fast__menu">
+                        <h4 className="pb-4">Categories</h4>
+                        <p>Men</p>
+                        <p>Woman</p>
+                        <p>Sunglasses</p>
+                        <p>Necklaces</p>
+                        <p>Sandals</p>
+                        <p>T-Shirt</p>
+                        <p>Parents</p>
+                        <p>Kids</p>
+                        <p>Jeans</p>
+                        <p>Jackets</p>
+                        <p>Blazer</p>
+                        <p>Luggage</p>
+                    </div>
+                    <div className="flex flex-col justify-between pb-20 fast__menu">
+                        <h4 className="pb-4">Brand</h4>
+                        <p>Ken Munk</p>
+                        <p>Artistoys</p>
+                        <p>Simon * ME</p>
+                        <p>WlideSkin</p>
+                        <p>PooMS</p>
+                        <p>mimibobot</p>
+                        <p>2K</p>
+                        <p>Jamungo</p>
+                        <p>AIAIAI</p>
+                        <p>Die Gestalten Verlag</p>
+                        <p>Arkitip</p>
+                    </div>
+                    <div className="flex flex-col justify-between pb-40 fast__menu">
+                        <h4 >Connect with Us</h4>
+                        <p>Email Us</p>
+                        <p>Facebook</p>
+                        <p>Twitter</p>
+                        <p>Youtube</p>
+                        <p>Radio</p>
+                        <p>Foursquare</p>
+                        <p>Newsletter</p>
+                        <p>RSS</p>
+                    </div>
+                </section>
             </footer>
             <Modal />
         </>
